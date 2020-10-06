f202d35f5d426a93f66095e2373400ba81f15955
M. Chae <cmirs56@gmail.com>
2020-10-06 21:06:41 +0900

    EDA

--- RSC/phychometrics.R
+++ RSC/phychometrics.R
@@ -4,4 +4,17 @@ train <- read_csv("DAT/train.csv")
 head(train)
 structure(train)
 summary(train)
+View(train)
 
+library(moonBook)
+mytable(voted ~ ., data = train) # Descriptive Statistics by 'voted' 
+
+library(PerformanceAnalytics)
+chart.Correlation(df_narm[,c(00:00)],pch=19) # Correlation of Qs
+
+library(ggplot2)
+colnames(train) # Select the EDA_features: age_group, education, engnat, familysize, gender, hand, married, race, religion
+
+ggplot(train, aes(x = voted)) +
+  geom_bar(position = "dodge")
+  
