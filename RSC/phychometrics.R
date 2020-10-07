6c48c5d343c02528d87cfa4afbcc5831218f42c9
M. Chae <cmirs56@gmail.com>
2020-10-07 20:48:29 +0900

    EDA

--- RSC/phychometrics.R
+++ RSC/phychometrics.R
@@ -9,12 +9,55 @@ View(train)
 library(moonBook)
 mytable(voted ~ ., data = train) # Descriptive Statistics by 'voted' 
 
-library(PerformanceAnalytics)
-chart.Correlation(df_narm[,c(00:00)],pch=19) # Correlation of Qs
+
+# data cleasing -----------------------------------------------------------
+
+# erase the index column
+
+
+# Visualization -----------------------------------------------------------
 
 library(ggplot2)
 colnames(train) # Select the EDA_features: age_group, education, engnat, familysize, gender, hand, married, race, religion
 
 ggplot(train, aes(x = voted)) +
   geom_bar(position = "dodge")
-  
+
+# Q_A_machiavellianism test score: boxplot
+
+
+# Q_E_consumed time for each Qs: boxplot 
+
+library(PerformanceAnalytics)
+chart.Correlation(df_narm[,c(00:00)],pch=19) # Correlation of Qs_positive vs. negative : grouping the Qs
+
+# age_group
+
+# education: 0 means no answer 
+
+# engnat: 1=Yes, 2=No, 0=n/a
+
+# familysize: 
+
+# gender 
+
+# hand: 1=Right, 2=Left, 3=Both, 0=n/a
+
+# married: 1=Never married, 2=Currently married, 3=Previously married, 0=Other
+
+# race: Asian, Arab, Black, Indigenous Australian, Native American, White, Other
+
+# religion: Agnostic, Atheist, Buddhist, Christian_Catholic, Christian_Mormon, Christian_Protestant, Christian_Other, Hindu, Jewish, Muslim, Sikh, Othe
+
+# tp01~10: grouping the items based on the correlation 
+
+chart.Correlation()
+
+# urban: 1=Rural, 2=Suburban, 3=Urban, 0=n/a
+
+# wr01~13: know the definition of real things: 1=Yes, 0=No
+
+# wf01~03: know the definition of fictitious things: 1=Yes, 0=No
+
+# voted: 1=Yes, 2=No 
+
